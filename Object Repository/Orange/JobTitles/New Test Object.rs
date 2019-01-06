be27ee18d7bfb530a9160218a52565573ac1a38b
Ebtehal Gamal Yusuf <Ebtehal Gamal Yusuf@LPALXY0199.ejadaalex.local>
2019-01-06 12:35:17 +0200

    Added new keyword

---
+++ Object Repository/Orange/JobTitles/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>5d91f6cf-a0d9-4456-9339-197abbfa5b55</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Test Suites/TS_Orange_JobTitles.ts
+++ Test Suites/TS_Orange_JobTitles.ts
@@ -19,7 +19,7 @@
    <testCaseLink>
       <guid>535c5eac-6d21-4c55-a11d-56507534df72</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Orange_Website/JobTitles_Screen</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
@@ -48,5 +48,17 @@
          <value>NOTE</value>
          <variableId>3a420513-d950-4e16-b01b-85a226339ad5</variableId>
       </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fdc551de-b078-4541-b1b4-945171e3efe3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Orange_Website/JobTitles_Screen2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>95774545-012a-438b-b2d1-0cf1098c2095</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Orange_Website/WebatableSearchtest</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
